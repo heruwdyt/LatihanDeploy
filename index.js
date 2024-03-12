e4e6f32a13e11d88ebd9a808c07038e81bf33cc9
unknown <rizkieoclub@gmail.com>
2024-03-12 11:57:08 +0700

    Latihan

---
+++ index.js
@@ -0,0 +1,14 @@
+// const Inputan = "Kevin" // itu tidak bisa diubah const tersebut karena nilainya tetap
+// contoh
+// const PHI = 3.14;
+// 2 * inputan * PHI
+
+
+
+$(document).ready(function(){
+    let h2 = $('h2')
+    console.log(h2.html("Selamat Datang"))
+})
+
+//Manipulasi text html di JavaScript
+ 
